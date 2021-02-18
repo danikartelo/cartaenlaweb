2e6b913baa8a05a7335e4b36dd7d49eeb9ad0512
danikartleo <daniel.teleco@gmail.com>
2021-02-18 23:37:41 +0000

    Creditos - INICIO

---
+++ load_master.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Daniel Rodríguez García"
+__copyright__ = "Copyright 2020, Caratenlaweb.com"
+__credits__ = ["Daniel Rodríguez García"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Daniel Rodríguez García"
+__email__ = ["info@cartaenlaweb.com","daniel.teleco@gmail.com"]
+__status__ = "Production"
+
+
+print("hola")
